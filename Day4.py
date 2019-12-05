a1740b739d5358e7ad427be0b0ef3de665fa8c7c
Tien Yin Yau <mail@TienYinYau.com>
2019-12-04 16:14:19 -0800

    finished part2

--- Day4.py
+++ Day4.py
@@ -19,6 +19,34 @@ def password_test(number):
 
    return 1 if adjacent and increment else 0
 
+def password_test2(number):
+   """
+   given a number, returns whether it follows the password rules
+   :param number: number to be tested
+   :return: bool
+   """
+   password=str(number)
+   adjacent=False
+   max_index=len(password) - 1
+   for i in range(0,max_index):
+       if (int(password[i+1])<int(password[i])):
+           return 0
+       if i==0:#no left-more position
+           if ((password[i] == password[i + 1])and
+                   (password[i+1]!=password[i+2])):
+               adjacent=True
+       elif i==max_index-1:
+           if((password[i]== password[i+1])and
+                   (password[i-1]!=password[i])):
+               adjacent=True
+       else:
+           if((password[i]==password[i+1])and
+                   (password[i-1]!=password[i])and
+                   (password[i+1]!=(password[i+2]))):
+               adjacent=True
+
+   return 1 if adjacent else 0
+
 def password_counter(min,max):
     """
     Works though given range of numbers.  If there are fewer than 3 items,
@@ -29,11 +57,11 @@ def password_counter(min,max):
     :return: number of passwords found within the range
     """
     if max-min==1:#two numbers are adjacent
-        return sum([password_test(min),password_test(max)])
+        return sum([password_test2(min),password_test2(max)])
     elif max-min==2:#three numbers in range
-        return sum([password_test(min),
-                   password_test(max-1),
-                   password_test(max)])
+        return sum([password_test2(min),
+                   password_test2(max-1),
+                   password_test2(max)])
     else:
         mid=(max-min)//2+min
         return sum([password_counter(min,mid),
@@ -50,6 +78,6 @@ if __name__=="__main__":
     number_range=data.split("-")
     min=int(number_range[0])
     max=int(number_range[1])
-
+    password_test2(168688)
     num_pass=password_counter(min,max)
     print(num_pass)
